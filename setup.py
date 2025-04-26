b5e621b3db436feca83e002d6440114b490d7609
Jalandhar Paswan <mrjalandharpaswan143@gmail.com>
2025-04-27 01:32:53 +0530

    Folder structure added!

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MediChat',
+    version= '0.0.0',
+    author= 'Jalandhar Paswan',
+    author_email= 'jalandharpaswan143@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
